ecb9d385919032a9b0be98ad7844515ce5588d8d
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-08-21 04:43:27 +0100

    Update 1-print_square.js

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -5,7 +5,7 @@
     The size of the square must be the first argument
     of the program.
 */
-// console.log(process.argv.length)
+
 
 if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
@@ -14,8 +14,7 @@ if (process.argv.length <= 2) {
     process.exit(1)
 }
 
-size = parseInt(process.argv[2])
-// console.log(size)
+size = parseInt(process.argv[2], 10)
 
 for (let i = 0 ; i < size ; i ++) {
     for (let j = 0 ; j < size ; j ++) {
